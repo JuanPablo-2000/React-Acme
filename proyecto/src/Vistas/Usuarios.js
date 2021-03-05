d7590f81b5fcbcec7bdb1551a7ba8bd3fe832b7a
JuanMillan7818 <juan.holgin@correounivalle.edu.co>
2021-03-04 22:48:13 -0500

    Correciones - Inicio

--- proyecto/src/Vistas/Usuarios.js
+++ proyecto/src/Vistas/Usuarios.js
@@ -1,51 +1,16 @@
-import React, { Component } from 'react';
-import $ from 'jquery'
-import '../css/style.css'
-import '../css/responsive.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../js/scripts'
-import {  faFacebookF, faTwitter, faInstagram, faPinterest, faFacebookMessenger} from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Component } from 'react'
+import Inicio from '../Componentes/Inicio-img'
 
-class UsuarioCliente extends Component{
+class UsuariosClientes extends Component{
     constructor(props){
         super();
     }    
 
-    modificarFondo() {     
-        $('#imagen-fondo').attr("id", "body-comun");                
-    }
-
-    hola(){
-        $(".tt-fullHeight").height($(window).height());
-    }
-
     render(){
-        this.modificarFondo();  
-        this.hola();      
-        return(               
-            <section id="home" className="tt-fullHeight" data-stellar-vertical-offset="50" data-stellar-background-ratio="0.2">
-                <div className="intro">
-                    <div className="intro-sub">Concesionario</div>
-                    <h1>Acme <span>Corporation</span></h1>
-                    <p></p>
-
-                    <div className="social-icons">
-                        <ul className="list-inline">
-                        <li><a href="#"><i className="fa"><FontAwesomeIcon icon={faFacebookF} /></i></a></li>
-                        <li><a href="#"><i className="fa"><FontAwesomeIcon icon={faTwitter} /></i></a></li>
-                        <li><a href="#"><i className="fa"><FontAwesomeIcon icon={faInstagram} /></i></a></li>
-                        <li><a href="#"><i className="fa"><FontAwesomeIcon icon={faPinterest} /></i></a></li>
-                        <li><a href="#"><i className="fa"><FontAwesomeIcon icon={faFacebookMessenger} /></i></a></li>
-                        </ul>
-                    </div>
-                </div>	
-                <div className="mouse-icon">
-                    <div className="wheel"></div>
-                </div>	
-            </section>                        
+        return (
+            <Inicio />
         );
     }
-}
+};
 
-export default UsuarioCliente;
+export default UsuariosClientes;
