d011a549aaca31f555412b4356161feefd283749
JuanMillan7818 <juan.holgin@correounivalle.edu.co>
2021-03-07 19:37:27 -0500

    Historiales - FechasPublicacion

--- proyecto/src/Componentes/Rutas.js
+++ proyecto/src/Componentes/Rutas.js
@@ -6,6 +6,9 @@ import CrudVehiculos from '../Vistas/CrudVehiculos'
 import Propietarios from '../Vistas/Propietarios'
 import Historiales from '../Vistas/Historiales'
 import Usuarios from '../Vistas/Usuarios';
+import HistorialCompra from '../Vistas/HistorialCompra'
+import HistorialOtro from '../Vistas/HistorialComparacion'
+import Publicaciones from '../Vistas/Publicaciones'
 
 function Rutas()  {
     return(        
@@ -18,6 +21,9 @@ function Rutas()  {
                 <Route path="/CrudVehiculos" component={CrudVehiculos} />           
                 <Route path="/Propietarios" component={Propietarios} /> 
                 <Route path="/Historial-venta" component={Historiales} /> 
+                <Route path="/Historial-compra" component={HistorialCompra} /> 
+                <Route path="/Historial-otros" component={HistorialOtro} /> 
+                <Route path="/Publicaciones" component={Publicaciones} /> 
             </Switch>
         </BrowserRouter>
     );  
--- proyecto/src/Vistas/HistorialComparacion.js
+++ proyecto/src/Vistas/HistorialComparacion.js
@@ -2,7 +2,7 @@ import React, { Component, useState } from 'react'
 
 import Inicio from '../Componentes/Inicio-img'
 import Fechas from '../Componentes/Fechas'
-import Grafica from '../Componentes/Grafica'
+import Grafica from '../Componentes/Grafica2'
 // ------------------------ JQuery ------------------------------------
 import $ from 'jquery'
 
@@ -15,7 +15,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // ------------------------ Imagenes -----------------------------------
 
-class Historial extends Component {
+class HistorialCompra extends Component {
     constructor(props) {
         super(props);        
     }     
@@ -91,109 +91,7 @@ class Historial extends Component {
                 </ul>            
             
         );
-    }
-
-    carrosMasComprados() {
-        return(
-            <section id="gallery">
-                <div className="container">
-                    <div className="row justify-content-around">
-                        <div className="col-lg-5 mb-4 col-md-4">
-                            <div className="blog-card spring-fever">
-                                <div className="title-content">
-                                    <h4 className="tituloCarro">Ford F-Series 47.517</h4>
-                                    <hr />                                    
-                                </div>
-                                <div className="card-info">
-                                    Cuando el camino se complica, un verdadero aventurero lo sabe sortear.                                    
-                                    <ul className="list-inline small">
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStar}/></i>
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStar}/></i>
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStar}/></i>
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStar}/></i>
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStar}/></i>
-                                    </ul>                        
-                                </div>
-                                <div className="utility-info">
-                                    <ul className="utility-list">
-                                    <li className="comments">12</li>
-                                    <li className="date">03.12.2015</li>
-                                    </ul>
-                                </div>                        
-                                <div className="gradient-overlay"></div>
-                                <div className="color-overlay"></div>
-                            </div>
-                        </div>
-                        <div className="col-lg-5 mb-4 col-md-4">
-                            <div className="blog-card spring-fever">
-                                <div className="title-content">
-                                    <h4 className="tituloCarro">Chevrolet Silverado 36.600</h4>
-                                    <hr />                                    
-                                </div>
-                                <div className="card-info">                                    
-                                    Robusta. Inteligente. Más grande, aunque más liviana que la generación anterior. 
-                                    <ul className="list-inline small">
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStar}/></i>
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStar}/></i>
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStar}/></i>
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStar}/></i>
-                                        <i className="fa text-success list-inline-item">
-                                            <FontAwesomeIcon icon={faStarHalfAlt}/></i>
-                                    </ul>                        
-                                </div>
-                                <div className="utility-info">
-                                    <ul className="utility-list">
-                                    <li className="comments">12</li>
-                                    <li className="date">03.12.2015</li>
-                                    </ul>
-                                </div>                        
-                                <div className="gradient-overlay"></div>
-                                <div className="color-overlay"></div>
-                            </div>
-                        </div>
-                        <div className="col-lg-5 mb-4 col-md-4">
-                            <div className="blog-card spring-fever">
-                                    <div className="title-content">
-                                        <h4 className="tituloCarro">RAM Pick Up 32.807</h4>
-                                        <hr />                                        
-                                    </div>
-                                    <div className="card-info">
-                                    Un interior orientado a la comodidad de todos los pasajeros y funciones pensadas en realizar el trabajo de forma eficiente.
-                                        <ul className="list-inline small">
-                                            <i className="fa text-dark list-inline-item">
-                                                <FontAwesomeIcon icon={faStar}/></i>
-                                            <i className="fa text-dark list-inline-item">
-                                                <FontAwesomeIcon icon={faStar}/></i>
-                                            <i className="fa text-dark list-inline-item">
-                                                <FontAwesomeIcon icon={faStar}/></i>
-                                            <i className="fa text-dark list-inline-item">
-                                                <FontAwesomeIcon icon={faStar}/></i>                                            
-                                        </ul>                                                                
-                                    </div>
-                                    <div className="utility-info">
-                                        <ul className="utility-list">
-                                        <li className="comments">12</li>
-                                        <li className="date">03.12.2015</li>
-                                        </ul>
-                                    </div>                        
-                                    <div className="gradient-overlay"></div>
-                                    <div className="color-overlay"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+    }  
 
     menuLateral() {
         return(
@@ -313,9 +211,8 @@ class Historial extends Component {
                         <br/> <br/>                                               
                         <Fechas />
                         <br/> <br/>    
-                        <h2>Historial de ventas</h2>
-                        <Grafica/>
-                        {this.carrosMasComprados()}                                                   
+                        <h2>Comparación</h2>
+                        <Grafica/>                                                                         
                     </div>                     
                 </div>                 
             </div>    
@@ -324,4 +221,4 @@ class Historial extends Component {
     }    
 };
 
-export default Historial;
+export default HistorialCompra;
