70456b0ec6b2b63a17c36470b59a6ce0345440a6
= <=>
2021-04-15 15:29:57 -0500

    BasedeDatos-Actualizacion-Campos

--- Backend/database/db.sql
+++ Backend/database/db.sql
@@ -13,7 +13,7 @@ CREATE TABLE clientes (
     fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
     fecha_actualizado DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
     activo BOOLEAN NOT NULL,
-    UNIQUE indice (id) ,
+    UNIQUE indice (id_persona) ,
     PRIMARY KEY(cedula)    
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8; /* Prueba no lo he ensayado*/
 
@@ -34,7 +34,7 @@ CREATE TABLE usuarios (
     cedula_id INT(12) NOT NULL ,
     nombre_usuario VARCHAR(50) NOT NULL ,
     contrasenia VARCHAR(100) NOT NULL , 
-    UNIQUE indice (id) ,
+    UNIQUE indice (id_usuario) ,
     PRIMARY KEY(nombre_usuario) ,     
     CONSTRAINT fk_cedula FOREIGN KEY (cedula_id) REFERENCES clientes(cedula)
 );
@@ -53,7 +53,7 @@ CREATE TABLE vehiculos (
     fecha_creado TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
     fecha_actualizado DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
     fecha_para_venta TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-    UNIQUE indice (id) ,
+    UNIQUE indice (id_vehiculo) ,
     PRIMARY KEY(placa) ,
     CONSTRAINT fk_ced_propietario FOREIGN KEY (cedula_propietario) REFERENCES clientes(cedula)
 );
@@ -67,10 +67,10 @@ CREATE TABLE compras (
     placa_vh_compra VARCHAR(50) NOT NULL,
     valor_compra DOUBLE(15,2) NOT NULL,
     fecha_compra TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,    
-    UNIQUE indice (id) , 
-    UNIQUE indice_1 (placa_vh) , 
+    UNIQUE indice (id_compra) , 
+    UNIQUE indice_1 (placa_vh_compra) , 
     PRIMARY KEY(serial_compra) ,    
-    CONSTRAINT fk_placa_compra FOREIGN KEY (placa_vh) REFERENCES vehiculos(placa) ,
+    CONSTRAINT fk_placa_compra FOREIGN KEY (placa_vh_compra) REFERENCES vehiculos(placa) ,
     CONSTRAINT fk_ced_cliente FOREIGN KEY (cedula_cliente) REFERENCES clientes(cedula)
 );
 
@@ -82,9 +82,9 @@ CREATE TABLE ventas (
     placa_vh_venta VARCHAR(50) NOT NULL ,
     valor_venta DOUBLE(15,2) NOT NULL ,
     fecha_venta_efectiva TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
-    UNIQUE indice (id) ,
-    UNIQUE indice_1 (placa_vh) ,
+    UNIQUE indice (id_venta) ,
+    UNIQUE indice_1 (placa_vh_venta) ,
     PRIMARY KEY(serial_venta) ,
     CONSTRAINT fk_propietario_nuevo FOREIGN KEY (propietario_nuevo) REFERENCES clientes(cedula) ,
-    CONSTRAINT fk_placa_venta FOREIGN KEY (placa_vh) REFERENCES vehiculos(placa)
+    CONSTRAINT fk_placa_venta FOREIGN KEY (placa_vh_venta) REFERENCES vehiculos(placa)
 );
